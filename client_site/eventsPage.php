3af9e1f34e035c9a54833f9385593972fde7eddd
Brandon Ezovski <ezov04@gmail.com>
2026-04-24 16:32:10 -0400

    added documentation for final submission

--- client_site/eventsPage.php
+++ client_site/eventsPage.php
@@ -24,7 +24,7 @@
 
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="event page for URI Gaming Club, listing all upcoming events and their details.">
+    <meta name="description" content="Event page for URI Gaming Club, listing all upcoming events and their details.">
     <meta charset="UTF-8">
     <title>URI Gaming Club Events</title>
     <link rel="stylesheet" href="css/style.css">
--- client_site/interestForm.php
+++ client_site/interestForm.php
@@ -176,7 +176,7 @@
 
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="interest form for URI Gaming Club to gather feedback and gauge interest in different types of events.">
+    <meta name="description" content="Interest form for URI Gaming Club to gather feedback and gauge interest in different types of events.">
     <meta charset="UTF-8">
     <title>URI Gaming Club Feedback Form</title>
     <link rel="stylesheet" href="css/style.css">
